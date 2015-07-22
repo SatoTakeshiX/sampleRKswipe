ab10df2625d5c72c12cc693105500c4f8fc8b8ec
satoutakeshi <sato.takeshi1987@gmail.com>
2015-07-23 02:13:48 +0900

    ストーリボードにてRKSwipeBetweenViewControllersを使えるようにしたデモプロジェクト

---
+++ sampleRKswipe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleRKswipe
+//
+//  Created by satoutakeshi on 2015/07/23.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleRKswipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleRKswipe
+//
+//  Created by satoutakeshi on 2015/07/23.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
